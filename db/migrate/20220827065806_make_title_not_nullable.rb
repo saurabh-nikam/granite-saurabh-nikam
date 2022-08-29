b2e9d83a15715a77e6205e1523a182d8eea859f4
Saurabh Nikam <saurabh.nikam@bigbinary.com>
2022-08-29 14:07:49 +0530

    Added slug to task

--- db/migrate/20220827065806_make_title_not_nullable.rb
+++ db/migrate/20220827065806_make_title_not_nullable.rb
@@ -1,4 +1,6 @@
-qqclass MakeTitleNotNullable < ActiveRecord::Migration[6.1]
+# frozen_string_literal: true
+
+class MakeTitleNotNullable < ActiveRecord::Migration[6.1]
   def change
     change_column_null :tasks, :title, false
   end
