f2d91167f8fdb599f7e4640fd817d3302d9c272c
Saurabh Nikam <saurabh.nikam@bigbinary.com>
2022-09-05 20:01:15 +0530

    Added tests for User model

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,7 @@ require "rails/test_help"
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
+  include ActionView::Helpers::TranslationHelper
   parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
