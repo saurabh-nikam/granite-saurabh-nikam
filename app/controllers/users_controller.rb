789ebc4fd25f5fec3a3fb0da403d8155e26258cf
Saurabh Nikam <saurabh.nikam@bigbinary.com>
2022-09-06 18:58:05 +0530

    fixed the tailwind css and users_controller file name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class UserController < ApplicationController
+class UsersController < ApplicationController
   skip_before_action :authenticate_user_using_x_auth_token, only: :create
   def index
     users = User.select(:id, :name)
